22933216d6355fe75b05a0524f277fbf326e1f32
Will <willnascimentowns@gmail.com>
2021-10-27 19:30:52 -0300

    Files Upload
    Upload of files from this project.
    
    technologies used: Html, Css, JavaScript with  jQuery.

---
+++ NETFLIX/arquivo.js
@@ -0,0 +1,19 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:30,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        300:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
